9e94350de074cdf3efcdb8176e9a3cabfcde4371
pythonincontainers <45329364+pythonincontainers@users.noreply.github.com>
2019-05-15 12:39:55 +0200

    Code sample reading Config params from various sources

---
+++ config-reader.py
@@ -0,0 +1,60 @@
+import json
+import argparse
+import os
+
+shape = ""
+color = ""
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser(description='Config reader example.')
+    arg_parser.add_argument('--color', nargs=1, help='set the color value')
+    arg_parser.add_argument('--shape', nargs=1, help='set the shape value')
+    arg_parser.add_argument('--file', nargs=1, help='read the configuration from file_name')
+    arg_parser.add_argument('--env', action='store_true', help='read the configuration from Environment Variables CONFIG_COLOR and CONFIG_SHAPE')
+    arguments = arg_parser.parse_args()
+
+    if arguments.env:
+        print('Reading configuration from Environment Variables')
+        try:
+            color = os.environ['CONFIG_COLOR']
+        except:
+            print('Error reading CONFIG_COLOR Variable')
+        try:
+            shape = os.environ['CONFIG_SHAPE']
+        except:
+            print('Error reading from CONFIG_SHAPE Variable')
+        print('"color" = ', color)
+        print('"shape" = ', shape)
+
+    if arguments.file:
+        filename = arguments.file[0]
+        print('Reading configuration from Config File ', filename)
+        try:
+            config_file = open(filename,"r")
+        except:
+            print('Error opening Config File ',filename)
+        else:
+            try:
+                config = json.load(config_file)
+            except:
+                print('Error reading JSON data from Config File')
+            else:
+                shape = config['shape']
+                color = config['color']
+                print('"color" = ', color)
+                print('"shape" = ', shape)
+
+    if arguments.color:
+        color = arguments.color[0]
+        print('Configuration from Command Line option --color ', color)
+
+    if arguments.shape:
+        shape = arguments.shape[0]
+        print('Configuration from Command Line option --shape ', shape)
+
+    if color == "" or shape == "":
+        print('Error: "color" and "shape" must be both defined and not empty')
+        arg_parser.print_help(file=None)
+        exit(1)
+    else:
+        print('Final configuration is: "color" = ', color, ', "shape" = ',shape)
